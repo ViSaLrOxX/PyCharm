68aa23937b0cd643109a4f9212d028b7291e27a3
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2024-05-05 15:40:35 +0530

    Revert "Add files via upload"

--- 2.py
+++
@@ -1,8 +0,0 @@
-def most_common_words (freqs) :
-    values = freqs.values ()
-    best = max(values)
-    words = []
-    for k in freqs:
-        if freqs[k] == best:
-            words. append (k)
-    return (words, best)
--- Problem set 0.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-#Asking user to enter the number “x”
-num_x = int(input("Enter number x: "))
-#Asking the user to enter the number “y”
-num_y = int(input("Enter number y: "))
-#Printing out number “x”, raised to the power “y”.
-print("x**y= ", num_x ** num_y)
-#Printing out the log (base 2) of “x”.
-print("log(x) =", numpy.log2(num_x))
